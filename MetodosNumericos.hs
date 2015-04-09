082ffbfc4b108aa5144005e883de22af39a9b1f9
Matías Mangiantini <matiasm15@hotmail.com>
2015-04-09 01:13:34 -0300

    Update MetodosNumericos.hs

--- MetodosNumericos.hs
+++ MetodosNumericos.hs
@@ -1,11 +1,10 @@
 
 biseccion f a b error limite | f a == 0 = a
                         	   | f b == 0 = b
-                         	   | otherwise = biseccionIterativo 0 f a b 0 error limite
+                         	   | otherwise = biseccionRecursivo 1 f a b error limite
 
-biseccionIterativo n f a b x error limite | (error > abs (b - a)) || (n > limite) = x
-                                          | mult > 0 = biseccionIterativo (n + 1) f c b c error limite
-                                          | mult < 0 = biseccionIterativo (n + 1) f a c c error limite
-                                          | otherwise = c
+biseccionRecursivo n f a b error limite | (mult == 0) || (error >= abs (b - a)) || (n >= limite) = c
+                                        | mult > 0 = biseccionRecursivo (n + 1) f c b error limite
+                                        | mult < 0 = biseccionRecursivo (n + 1) f a c error limite
   where mult = f a * f c
         c = (a + b) / 2
